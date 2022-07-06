fdb7859c76a070ec1ce8a5290828edca8df74ed9
emachipana <emachipana0102@gmail.com>
2022-07-06 18:04:32 -0500

    ADD: styles of button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import { colors } from "../../styles";
+
+export const StyledButton = styled.button`
+  max-width: 310px;
+  min-width: 300px;
+  max-height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 12px 1rem;
+  border: none;
+  border-radius: 30px;
+  background-color: ${colors.orange};
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 23px;
+  color: ${colors.white};
+  cursor: pointer;
+  transition: .2s ease-in;
+  &:active {
+    transform: translateY(5px);
+  }
+  &:disabled {
+    transform: none;
+    opacity: .6;
+    cursor: not-allowed;
+  }
+`;
