71f24ba00db761ae2b9ad547d29f0c96a5f7b1af
emachipana <emachipana0102@gmail.com>
2022-07-07 02:03:22 -0500

    ADD: involved in browser router of react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { global, reset } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Global styles={global}/>
-    <Global styles={reset}/>
-    <App />
+    <Global styles={global} />
+    <Global styles={reset} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
