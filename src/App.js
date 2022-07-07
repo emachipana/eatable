2252e02e5bd5f9a7683e16590dc6a0df5bb0b4d5
emachipana <emachipana0102@gmail.com>
2022-07-07 00:44:01 -0500

    ADD: render input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import styled from "@emotion/styled";
-import Button from "./components/Button";
-import CardItem from "./components/CardItem/card-item";
-import FoodCard from "./components/FoodCard";
-import Loader from "./components/Loader";
+import Input from "./components/Input";
 
 const Container = styled.div`
   margin: 5rem;
@@ -11,15 +8,7 @@ const Container = styled.div`
 function App() {
   return (
     <Container>
-      <Loader />
-      <FoodCard 
-        name="Veggie tomato" 
-        price="15.45" 
-        image="https://live.mrf.io/statics/i/ps/irecetasfaciles.com/wp-content/uploads/2019/01/pizza-con-salami-chorizo-beacon.jpg?width=1200&enable=upscale"
-      />
-      <Button>Login</Button>
-      <Button disabled>SignUp</Button>
-      <CardItem />
+      <Input label="Email" type="email" error="invalid format" isTouched/>
     </Container>
   );
 }
