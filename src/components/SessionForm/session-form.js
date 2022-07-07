09309decec9196c2928d85e214877932543272b5
emachipana <emachipana0102@gmail.com>
2022-07-07 01:31:25 -0500

    ADD: session form component

---
+++ src/components/SessionForm/session-form.js
@@ -0,0 +1,73 @@
+import { Formik } from "formik";
+import Button from "../Button";
+import Input from "../Input";
+import { Form, Container } from "./styles";
+
+function SessionForm({ handleSubmit, type }) {
+  function validate(values) {
+    const errors = {};
+
+    const { email, password } = values;
+    if(email === "") {
+      errors.email = "Email is required";
+    }else if(!/.+@.+\.[A-Za-z]+$/.test(email)) {
+      errors.email = "Format is invalid";
+    }
+
+    if(password === "") {
+      errors.password = "Password is required";
+    }else if(password.length < 6) {
+      errors.password = "At least 6 chacarters long";
+    }
+
+    return errors;
+  }
+
+  const initialForm = { email: "", password: "" }
+
+  return (
+    <Formik
+      initialValues={initialForm}
+      onSubmit={handleSubmit}
+      validate={validate}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isValid
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <Container>
+            <Input
+              type="email"
+              label="Email"
+              placeholder="example@mail.com"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.email}
+              isTouched={touched.email}
+            />
+            <Input 
+              type="password"
+              label="Password"
+              placeholder="**********"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.password}
+              isTouched={touched.password}
+            />
+          </Container>
+          <Button disabled={!isValid} type="submit">{type}</Button>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default SessionForm;
