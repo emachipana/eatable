125ff069615670f957537c2e89c79676dacc1eef
emachipana <emachipana0102@gmail.com>
2022-07-07 13:31:23 -0500

    ADD: partial authenticated app

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,5 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Loader from "./components/Loader";
+import { useProducts } from "./context/products-context";
+
 function AuthenticatedApp() {
-  return <h1>Welcome</h1>
+  const { isLoading } = useProducts();
+
+  return (
+    isLoading
+    ?
+    <Loader />
+    :
+    <Routes>
+      <Route index path="/" element={<Navigate to="/home" replace={true} />} />
+      <Route path="/home" element={<h1>Home Page</h1>}/>
+      <Route path="*" element={<h1>Page Not Found</h1>} />
+    </Routes>
+  );
 }
 
 export default AuthenticatedApp;
