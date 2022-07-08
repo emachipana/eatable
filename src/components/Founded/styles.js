c798e3f8cadab238b2cb5eaa7f0f693bf3255721
emachipana <emachipana0102@gmail.com>
2022-07-08 18:47:41 -0500

    ADD: styles of founded

---
+++ src/components/Founded/styles.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  margin: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  height: 65vh;
+`;
+
+export const Title = styled.h1`
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 35px;
+  text-align: center;
+`;
