1bf042b006d47e49d9ac4095fb1ac87820f11290
emachipana <emachipana0102@gmail.com>
2022-07-08 01:04:34 -0500

    ADD: render partial components in home page

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import Search from "../components/Search";
+import SlideMenu from "../components/SlideMenu";
+import { useProducts } from "../context/products-context";
 
 function HomePage() {
-  return <Search />
+  const { products } = useProducts();
+  const options = Object.keys(products);
+  const [currentPage, setCurrentPage] = useState(options[0]);
+  const [currentProducts, setCurrentProducts] = useState(products);
+
+  return (
+    <>
+      <Search />
+      <SlideMenu 
+        options={[...options]}
+        currentOption={currentPage}
+        setCurrentOption={setCurrentPage}
+      />
+    </>
+  );
 }
 
 export default HomePage;
