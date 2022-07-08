1e245fbd050ec392436373af1317c461a116936a
emachipana <emachipana0102@gmail.com>
2022-07-08 12:58:51 -0500

    ADD: logic to search products

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -5,24 +5,60 @@ import SlideMenu from "../components/SlideMenu";
 import { useProducts } from "../context/products-context";
 
 function HomePage() {
-  const { products } = useProducts();
+  const { products, searchProducts } = useProducts();
   const options = Object.keys(products);
   const [currentPage, setCurrentPage] = useState(localStorage.getItem("current_page") || options[0]);
   const [currentProducts, setCurrentProducts] = useState([]);
+  const [search, setSearch] = useState({
+    isSearching: false,
+    query: ""
+  })
 
   useEffect(() => {
     setCurrentProducts(products[currentPage]);
     localStorage.setItem("current_page", currentPage);
-  }, [currentPage, products])
+  }, [currentPage, products]);
+
+  const { isSearching, query } = search;
+
+  function handleChange(value) {
+    setSearch({...search, query: value });
+  }
+
+  function back() {
+    setSearch({ query: "", isSearching: false })
+  }
+
+  useEffect(() => {
+    const isSearching = query !== "";
+    setSearch({ query: query, isSearching: isSearching });
+    if(isSearching) {
+      setCurrentProducts(searchProducts(query));
+    }else {
+      setCurrentProducts(products[currentPage]);
+    }
+
+  }, [query, products, currentPage, searchProducts]);
 
   return (
     <>
-      <Search />
-      <SlideMenu 
-        options={[...options]}
-        currentOption={currentPage}
-        setCurrentOption={setCurrentPage}
+      <Search 
+        isSearching={isSearching}
+        value={query}
+        handleChange={handleChange}
+        handleClick={back}
       />
+      {
+        isSearching
+        ?
+        <p>Founded {currentProducts.length} products</p>
+        :
+        <SlideMenu 
+          options={[...options]}
+          currentOption={currentPage}
+          setCurrentOption={setCurrentPage}
+        />
+      }
       <ListProducts 
         products={currentProducts}
       />
