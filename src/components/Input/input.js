6af2051d17017b1c0b172b0375966cb7839ed506
emachipana <emachipana0102@gmail.com>
2022-07-09 10:48:56 -0500

    ADD: other props

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,23 +1,34 @@
 import * as Style from "./styles";
 
-function Input({ label, type, value, onChange, onBlur, error, isTouched, placeholder }) {
-  const labelParsed = label.toLowerCase().replace(/ /g, "_");
-  
-  return (
-    <Style.Container>
-      <Style.Label htmlFor={labelParsed}>{ label }</Style.Label>
-      <Style.StyledInput
-        type={type || "text"}
-        id={labelParsed}
-        name={labelParsed}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-        placeholder={placeholder}
-      />
-      { error && isTouched && <Style.ErrorMessage>{ error }</Style.ErrorMessage>}
-    </Style.Container>
-  );
+function Input({ 
+  label="",
+  type,
+  value,
+  onChange,
+  onBlur,
+  error,
+  isTouched,
+  placeholder,
+  ...others
+  }) {
+    const labelParsed = label.toLowerCase().replace(/ /g, "_");
+    
+    return (
+      <Style.Container>
+        <Style.Label htmlFor={labelParsed}>{ label }</Style.Label>
+        <Style.StyledInput
+          type={type || "text"}
+          id={labelParsed}
+          name={labelParsed}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          placeholder={placeholder}
+          {...others}
+        />
+        { error && isTouched && <Style.ErrorMessage>{ error }</Style.ErrorMessage>}
+      </Style.Container>
+    );
 }
 
 export default Input;
