f52f0cab416f3f356179344662afda55d9384553
emachipana <emachipana0102@gmail.com>
2022-07-08 23:47:50 -0500

    FIX: size of footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,12 +4,12 @@ import { colors } from "../../styles";
 export const Container = styled.div`
   position: fixed;
   bottom: 0;
-  width: 100vw;
+  width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1rem 3rem;
-  height: 60px;
+  height: 40px;
   background-color: ${colors.gray.light};
   z-index: 1;
 `;
