fa70ff4b1c0ffe79ceeda33a8ec5977c88a212ce
emachipana <emachipana0102@gmail.com>
2022-07-09 10:52:55 -0500

    ADD: route to edit page

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -4,6 +4,7 @@ import Loader from "./components/Loader";
 import { useProducts } from "./context/products-context";
 import HomePage from "./pages/home-page";
 import ProductPage from "./pages/product-page";
+import ProfilePage from "./pages/profile-page";
 
 function AuthenticatedApp() {
   const { isLoading } = useProducts();
@@ -17,7 +18,9 @@ function AuthenticatedApp() {
       <Routes>
         <Route index path="/" element={<Navigate to="/home" replace={true} />} />
         <Route path="/home" element={<HomePage />}/>
-        <Route path="/product/:id" element={<ProductPage />}/>
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile/edit" element={<h1>Edit page</h1>}/>
         <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
       <Footer />
