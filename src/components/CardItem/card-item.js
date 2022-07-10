94f2ee06b73e8076d91ae6e17f6b89f7ab58c294
emachipana <emachipana0102@gmail.com>
2022-07-09 19:21:48 -0500

    FIX: filter products

--- src/components/CardItem/card-item.js
+++ src/components/CardItem/card-item.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { useProducts } from "../../context/products-context";
+import { useUser } from "../../context/user-context";
 import Counter from "../Counter";
 import * as Style from "./styles";
 
 function CardItem({id, currentValue, handleProduct}) {
   const { findProduct } = useProducts();
+  const { user } = useUser();
 
   const product = findProduct(id);
   const products = JSON.parse(localStorage.getItem("cartProducts"));
-  const productCart = products.find(product => product.id === id);
+  const filterProducts = products.filter(product => product.user === user.email);
+  const productCart = filterProducts.find(product => product.id === id);
   const index = products.findIndex(product => product.id === id);
 
   const [counter, setCounter] = useState(currentValue || 1);
@@ -16,24 +19,23 @@ function CardItem({id, currentValue, handleProduct}) {
   function handleIncrease() {
     setCounter(counter + 1);
     productCart.quantity = counter + 1;
-    products[index] = productCart;
-    localStorage.setItem("cartProducts", JSON.stringify(products));
-    handleProduct(products);
+    filterProducts[index] = productCart;
+    localStorage.setItem("cartProducts", JSON.stringify([...products, filterProducts]));
+    handleProduct(filterProducts);
   }
 
   function handleDecrease() {
     if(counter <= 1) {
-      const newProducts =  products.filter(product => product.id !== id);
-      localStorage.setItem("cartProducts", JSON.stringify(newProducts));
+      const newProducts = filterProducts.filter(product => product.id !== id);
+      localStorage.setItem("cartProducts", JSON.stringify([...products, newProducts]));
       handleProduct(newProducts);
       return;
     };
-    
     setCounter(counter - 1);
     productCart.quantity = counter - 1;
-    products[index] = productCart;
-    localStorage.setItem("cartProducts", JSON.stringify(products));
-    handleProduct(products);
+    filterProducts[index] = productCart;
+    localStorage.setItem("cartProducts", JSON.stringify([...products, filterProducts]));
+    handleProduct(filterProducts);
   }
 
   return (
