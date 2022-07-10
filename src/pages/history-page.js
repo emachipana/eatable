e5b7d3e4ec50a0586cf75724363fe55b53d25828
emachipana <emachipana0102@gmail.com>
2022-07-09 22:53:18 -0500

    render order card

--- src/pages/history-page.js
+++ src/pages/history-page.js
@@ -2,6 +2,7 @@ import { Container, Header, IconStyle, MainContainer, Title } from "./styles";
 import { AiOutlineLeft } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import Founded from "../components/Founded";
+import OrderCard from "../components/OrderCard/order-card";
 
 function HistoryPage({ orders }) {
   const navigate = useNavigate();
@@ -24,7 +25,16 @@ function HistoryPage({ orders }) {
                 type="history"
               />
               :
-              <h1>hello</h1>
+              orders.map(order => (
+                <OrderCard
+                  key={order.id}
+                  date={order.created_at}
+                  items={order.items_count}
+                  total={order.total}
+                  details={order.order_details}
+                  address={order.delivery_address}
+                />
+              ))
             }
       </Container>
     </MainContainer>
