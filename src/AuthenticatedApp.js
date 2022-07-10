5e34ceeb8d0a5961c27d960c15f9035d490ccad1
emachipana <emachipana0102@gmail.com>
2022-07-09 23:49:26 -0500

    render checkout

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Loader from "./components/Loader";
 import { useProducts } from "./context/products-context";
 import CartPage from "./pages/cart-page";
+import CheckoutPage from "./pages/checkout-page";
 import EditPage from "./pages/edit-page";
 import HistoryPage from "./pages/history-page";
 import HomePage from "./pages/home-page";
@@ -14,6 +15,7 @@ import { getOrders } from "./services/order-service";
 function AuthenticatedApp() {
   const { isLoading } = useProducts();
   const [orders, setOrders] = useState([]);
+  const [newOrder, setNewOrder] = useState({});
   
   useEffect(() => {
     getOrders().then(data => setOrders(data)).catch(console.error);
@@ -31,8 +33,9 @@ function AuthenticatedApp() {
         <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile/edit" element={<EditPage />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route path="/cart" element={<CartPage setNewOrder={setNewOrder}/>} />
         <Route path="/history" element={<HistoryPage orders={orders}/>} />
+        <Route path="/checkout" element={<CheckoutPage newOrder={newOrder} orders={orders} setOrders={setOrders}/>} />
         <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
       <Footer />
